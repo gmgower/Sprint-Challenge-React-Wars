ac67aad68cb2a6959943a94f6a343b3fd7bbeda6
Mada Gower <gmgower@gmail.com>
2019-08-02 21:00:14 -0500

    Add grid styles to card.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,52 +1,46 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import StarWarsCharacter from './components/StarWarsCharacter.component';
 
 import axios from 'axios';
 
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-const [dataState, setDataState] = useState([])
+  const [dataState, setDataState] = useState([]);
 
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people').then(res => {
+      console.log(res.data);
+      setDataState(res.data.results);
+    });
+  }, []);
 
-useEffect(() => {
-  axios.get("https://swapi.co/api/people")
-  .then(res => {
-    console.log(res.data)
-    setDataState(res.data.results)
-        })
-}, [])
-
-
-
- 
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-
-      <div>
-     {dataState.map(data =>
-    <StarWarsCharacter 
-    name={data.name}
-    birth={data.birth_year}
-    gender={data.gender}
-    eye={data.eye_color}
-    hair={data.hair_color}
-    height={data.height}
-    mass={data.mass}
-    skin={data.skin_color}    
-     />
-    )}   
+    <div className='App'>
+      <h1 className='Header'>React Wars</h1>
+      <div className='card-list'>
+        {dataState.map(data => (
+          <StarWarsCharacter
+            name={data.name}
+            birth={data.birth_year}
+            gender={data.gender}
+            eye={data.eye_color}
+            hair={data.hair_color}
+            height={data.height}
+            mass={data.mass}
+            skin={data.skin_color}
+          />
+        ))}
+      </div>
     </div>
-    </div>
-
-);
-}
+  );
+};
 
 export default App;
--- starwars/src/components/StarWarsCharacter.component.jsx
+++ starwars/src/components/StarWarsCharacter.component.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './StarWars.styles.css';
+import {} from 'semantic-ui-react';
+import styled from 'styled-components';
+import './StarWars.styled.js';
 
-function StarWarsCharacter (props) {
-    console.log(props)
-    return (
-        <div>
-            <h1>
-            {props.name}
-            </h1>
-        <ul>
-            <li>{props.birth}</li>
-            <li>{props.gender}</li>
-            <li>{props.eye}</li>
-            <li>{props.hair}</li>
-            <li>{props.height}</li>
-            <li>{props.mass}</li>
-            <li>{props.skin}</li>
-        </ul>
-        </div>
-    )
+function StarWarsCharacter(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <ul>
+        <li>{props.birth}</li>
+        <li>{props.gender}</li>
+        <li>{props.eye}</li>
+        <li>{props.hair}</li>
+        <li>{props.height}</li>
+        <li>{props.mass}</li>
+        <li>{props.skin}</li>
+      </ul>
+    </div>
+  );
 }
 
-export default StarWarsCharacter 
+export default StarWarsCharacter;
