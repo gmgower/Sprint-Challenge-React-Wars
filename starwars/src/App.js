f8a0fe9165702f025a367e51cd10dbe58ccd00d4
Mada Gower <gmgower@gmail.com>
2019-08-02 20:33:52 -0500

    Add useState, useEffect, grab data & map over to render to browser.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 import StarWarsCharacter from './components/StarWarsCharacter.component';
@@ -12,17 +12,33 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const [dataState, setDataState] = useState([])
+
+
+useEffect(() => {
   axios.get("https://swapi.co/api/people")
   .then(res => {
     console.log(res.data)
-    
-  })
+    setDataState(res.data.results)
+        })
+}, [])
+
+
+
+ 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <StarWarsCharacter name="Props Name" />
+
+      <div>
+     {dataState.map(data =>
+    <StarWarsCharacter name={data.name} />
+
+    )}   
     </div>
-  );
+    </div>
+
+);
 }
 
 export default App;
